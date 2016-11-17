90b87efecdba002c5eb71291ab1f28a2b5a0c86d
Damir2805 <d_kalymbaev@mail.ru>
2016-11-17 18:54:28 +0300

    the newest version

---
+++ Chapter_001/src/main/java/dkalymbaev/Calculate.java
@@ -0,0 +1,22 @@
+package dkalymbaev;
+
+/**
+ * Calculate.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Calculate {
+	public static void main(String[] args) {
+		
+		 /**
+     * Main.
+     * @param args - args.
+     */
+	 
+		System.out.println("Hello World!");
+	}
+	
+}
