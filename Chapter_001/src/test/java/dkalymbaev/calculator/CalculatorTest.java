59a57cec0ff8c2d0354350e410e2608b1e60b5c9
Damir2805 <d_kalymbaev@mail.ru>
2016-12-12 15:33:53 +0300

    new function

---
+++ Chapter_001/src/test/java/dkalymbaev/calculator/CalculatorTest.java
@@ -0,0 +1,86 @@
+package dkalymbaev;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static java.lang.Math.pow;
+
+/**
+* Created by Damir Kalymbaev.
+*/
+public class CalculatorTest {
+	/**
+	* Testing a sum.
+	*/
+	@Test
+	public void whenSumCalculated() {
+		Calculator calculator = new Calculator();
+		final double one = 5;
+		final double two = 2;
+		final double three = 7;
+		calculator.add(one, two);
+		double result = calculator.getResult();
+		assertThat(result, is(three));
+	}
+	/**
+	* Testing a substraction.
+	*/
+	@Test
+	public void whenSubstractionCalculated() {
+		Calculator calculator = new Calculator();
+		final double one = 5;
+		final double two = 2;
+		final double three = 3;
+		calculator.substract(one, two);
+		double result = calculator.getResult();
+		assertThat(result, is(three));
+	}
+	/**
+	* Testing a multiplication.
+	*/
+	@Test
+	public void whenMultiplicationCalculated() {
+		Calculator calculator = new Calculator();
+		final double one = 5;
+		final double two = 2;
+		final double three = 10;
+		calculator.multiple(one, two);
+		double result = calculator.getResult();
+		assertThat(result, is(three));
+	}
+	/**
+	* Testing a division.
+	*/
+	@Test
+	public void whenDivisionCalculated() {
+		Calculator calculator = new Calculator();
+		final double one = 6;
+		final double two = 2;
+		final double three = 3;
+		calculator.div(one, two);
+		double result = calculator.getResult();
+		assertThat(result, is(three));
+	}
+	/**
+	* Testing an expansion.
+	*/
+	@Test
+	public void whenExpandIsCalculated() {
+		Calculator calculator = new Calculator();
+		final double one = 6;
+		final double two = 2;
+		final double three = 36;
+		calculator.expand(Math.pow(one, two));
+		double result = calculator.getResult();
+		assertThat(result, is(three));
+	}
+	/**
+	* Testing a getting result.
+	*/
+	@Test
+		public void whenGetResultWorksThenReturnItValue() {
+		Calculator calculator = new Calculator();
+		final double expected = 0.0;
+		assertThat(calculator.getResult(), is(expected));
+    }
+}
