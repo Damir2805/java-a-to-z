cd66044da3e0f286b80cef71dddc03402778023e
Damir2805 <d_kalymbaev@mail.ru>
2016-12-20 18:45:23 +0300

    add a new pointtest.java file

---
+++ Chapter_001/src/test/java/dkalymbaev/triangle/PointTest.java
@@ -0,0 +1,22 @@
+package dkalymbaev;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+* Testing of distance between two points.
+*/
+
+public class PointTest {
+	/**
+	* Defining two points
+	*/
+	final Point point1 = new Point(0, 1);
+	final Point point2 = new Point(0, 5);
+	final Space12 = point1.distanceTo(point2);
+	@Test
+	public void distsnceToTest() {
+		assertThat(Space12, is(4d));
+	}
+}
